5a2e9cc99a68b92f74d120aaf10f32f50c0260e1
mugworks <greenwood.pdx@gmail.com>
2017-02-21 16:19:32 -0800

    cleaned up my constructors. Added chart info.

--- app.js
+++ app.js
@@ -4,84 +4,111 @@ var totalClicks = 0; //keeps track of # times user has clicked on images
 
 
 
-function ProductImage(name, filePath, numShown, numClicks) {
-  this.name = name;
-  this.filePath = filePath;
-  this.numShown = numShown;
-  this.numClicks = numClicks;
+function ProductImage(fileName) {
+  this.name = fileName.split('.')[0];
+  this.filePath = 'images/' + fileName;
+  this.numShown = 0;
+  this.numClicks = 0;
 }
 
-var product1 = new ProductImage('bag', 'images/bag.jpg', 0, 0);
-var product2 = new ProductImage('banana', 'images/banana.jpg', 0, 0);
-var product3 = new ProductImage('bathroom', 'images/bathroom.jpg', 0, 0);
-var product4 = new ProductImage('boots', 'images/boots.jpg', 0, 0);
-var product5 = new ProductImage('breakfast', 'images/breakfast.jpg', 0, 0);
-var product6 = new ProductImage('bubblegum', 'images/bubblegum.jpg', 0, 0);
+var fileNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg'];
+var productArray = [];
+for (var i = 0; i < fileNames.length; i++) {
+  productArray.push(new ProductImage(fileNames[i]));
+}
 
 var imgTag1 = document.getElementById('one');
 var imgTag2 = document.getElementById('two');
 var imgTag3 = document.getElementById('three');
 
-var productArray = [product1, product2, product3, product4, product5, product6];
 
-var array = [];
-var rand;
-for (var y = 0; y < productArray.length; y++) {
-  array[y] = y;
-}
-rand =  Math.floor(Math.random() * array.length);
-var j = array[rand];
-array.splice(rand,1);
-rand =  Math.floor(Math.random() * array.length);
-var k = array[rand];
-array.splice(rand,1);
-rand =  Math.floor(Math.random() * array.length);
-var m = array[rand];
-// checkNumbers();
-imgTag1.setAttribute ('src', productArray[j].filePath);
-imgTag2.setAttribute ('src', productArray[k].filePath);
-imgTag3.setAttribute ('src', productArray[m].filePath);
-console.log(j);
-console.log(k);
-console.log(m);
-var previousArray = [j, k, m];
-console.log(previousArray, 'here');
 
-// function checkNumbers() {
-//   if (j === k || j === m || k === m) {
-//     getThreeImages();
-//   };
+// var array = [];
+// var rand;
+// for (var y = 0; y < productArray.length; y++) {
+//   array[y] = y;
+// }
+// rand = Math.floor(Math.random() * array.length);
+// var j = array[rand];
+// array.splice(rand,1);
+// rand = Math.floor(Math.random() * array.length);
+// var k = array[rand];
+// array.splice(rand,1);
+// rand = Math.floor(Math.random() * array.length);
+// var m = array[rand];
+// imgTag1.setAttribute ('src', productArray[j].filePath);
+// imgTag2.setAttribute ('src', productArray[k].filePath);
+// imgTag3.setAttribute ('src', productArray[m].filePath);
+// console.log(j);
+// console.log(k);
+// console.log(m);
+// var previousArray = [j, k, m];
+// console.log(previousArray, 'here');
+// var large;
+// var medium;
+// var small;
+// for (y = 0; y < productArray.length; y++) {  //new code for previous array
+//   array[y] = y;
+// }
+// if (j > k) {
+//   if (j > m) {
+//     large = j;
+//     if (k > m) {
+//       medium = k;
+//       small = m;
+//     } else {
+//       medium = m;
+//       small = k;
+//     }
+//   } else if (k > m){
+//     large = k;
+//     if (m > j) {
+//       medium = m;
+//       small = j;
+//     } else {
+//       medium = j;
+//       small = m;
+//     }
+//   }
 // }
 
+
+
+getThreeImages();
 function getThreeImages() {
-  // j = Math.floor(Math.random() * productArray.length);
-  // k = Math.floor(Math.random() * productArray.length);
-  // m = Math.floor(Math.random() * productArray.length);
-var array = [];
-var rand;
-for (var y = 0; y < productArray.length; y++) {
-  array[y] = y;
-}
-rand =  Math.floor(Math.random() * array.length);
-var j = array[rand];
-array.splice(rand,1);
-rand =  Math.floor(Math.random() * array.length);
-var k = array[rand];
-array.splice(rand,1);
-rand =  Math.floor(Math.random() * array.length);
-var m = array[rand];
+  var array = [];
+  var rand;
+  for (var y = 0; y < productArray.length; y++) {
+    array[y] = y;
+  }
+  rand = Math.floor(Math.random() * array.length);
+  var j = array[rand];
+  array.splice(rand,1);
+  rand = Math.floor(Math.random() * array.length);
+  var k = array[rand];
+  array.splice(rand,1);
+  rand = Math.floor(Math.random() * array.length);
+  var m = array[rand];
   console.log(j);
-console.log(k);
-console.log(m);
-setAttributes(j, k, m);
-// previousArray = [j, k, m];
-console.log(previousArray);
+  console.log(k);
+  console.log(m);
+  setAttributes(j, k, m);
+  incrementNumShown(j, k, m);
+  // previousArray = [j, k, m];
+  // console.log(previousArray);
 }
 function setAttributes(j, k, m) {
   imgTag1.setAttribute ('src', productArray[j].filePath);
   imgTag2.setAttribute ('src', productArray[k].filePath);
   imgTag3.setAttribute ('src', productArray[m].filePath);
 }
+function incrementNumShown(j, k, m) {
+  productArray[j].numShown++;
+  productArray[k].numShown++;
+  productArray[m].numShown++;
+}
+
+
 function finished() {
   console.log('done');
 }
@@ -107,3 +134,36 @@ imgTag1.addEventListener('click', clickHandler);
 imgTag2.addEventListener('click', clickHandler);
 imgTag3.addEventListener('click', clickHandler);
 
+var ctx = document.getElementById('selectionChart');
+
+var chartConfig = {
+  type: 'bar',
+  data: {
+    labels: ['bag', 'banana', 'bathroom'],  //put the image names here
+    datasets: [{
+      label: 'times clicked',
+      data: [3, 7, 4],        //put the number of times clicked here
+      backgroundColor: [
+        'rgba(255, 0, 0, .5)', //figure out how to alternate colors
+        'rgba(0, 255, 0, .5)',
+        'rgba(0, 0, 255, .5)'
+      ],
+      borderColor: [
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)',
+        'rgba(0, 0, 0, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+};
+var renderedChart = new Chart(ctx, chartConfig);
